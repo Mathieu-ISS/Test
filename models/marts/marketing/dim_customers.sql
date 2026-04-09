30b0778db38c941e691607d4f1c5da115396dcb0
mathieu.issindou@gmail.com <mathieu.issindou@gmail.com>
2026-04-09 15:54:32 +0000

    Honestly I did way too many things on this one...

---
+++ models/marts/marketing/dim_customers.sql
@@ -0,0 +1,55 @@
+with customers as (
+
+     select * from {{ ref('stg_jaffle_shop__customers') }}
+
+),
+
+orders as ( 
+
+    select * from {{ ref('stg_jaffle_shop__orders') }}
+
+),
+
+successful_payments as (
+
+    select * 
+    from {{ ref('stg_stripe__payments')}}
+    where status = "success"
+),
+
+customer_orders as (
+
+    select
+        customer_id,
+        min(order_date) as first_order_date,
+        max(order_date) as most_recent_order_date,
+        count(order_id) as number_of_orders,
+        sum(amount / 100) as amount
+
+    from orders
+    left join successful_payments
+    on orders.order_id = successful_payments.orderid
+
+    group by 1
+
+),
+
+final as (
+
+    select
+        customers.customer_id,
+        customers.first_name,
+        customers.last_name,
+        customer_orders.first_order_date,
+        customer_orders.most_recent_order_date,
+        coalesce(customer_orders.number_of_orders, 0) -- Coalesce transforme les valeurs de commande "null" en valeur 0.
+        as number_of_orders,
+        customer_orders.amount
+
+    from customers
+
+    left join customer_orders using (customer_id)
+
+)
+
+select * from final
---
+++ models/staging/stripe/stg_stripe__payments.sql
@@ -0,0 +1,5 @@
+select *
+
+from dbt-tutorial.stripe.payment
+
+order by orderid
